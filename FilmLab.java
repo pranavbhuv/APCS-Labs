307ff52a61b996f64363b39065ba7acab07f5fb9
QuiverlyRivalry <dungeonwolfmc@gmail.com>
2020-01-26 19:51:28 -0600

    hullo world

---
+++ FilmLab.java
@@ -0,0 +1,59 @@
+public class Filmst
+{
+  public static void main(String [] args)
+  {
+    
+    Film[] watch = new Film[4];
+    
+    watch[0] = new Film("Shrek", 133, "PG");
+    watch[1] = new Film("Road to Perdition", 117, "G");
+    watch[2] = new Film("The Truth about Cats and Dogs", 03, "PG");
+    watch[3] = new Film("Enigma", 114, "G");
+    
+    for(int i = 0; i < watch.length; i++)
+    {
+     	watch[i].setLength(10);
+    }
+    
+    // Practice advanced loop
+    
+    for(Film mov: watch)
+    {
+      mov.display();
+      System.out.println("\t\t");
+    }
+    
+    // 100 Point
+    Film shortestFilm = new Film("Cats", 5, "G");
+    watch[2] = shortestFilm;
+    System.out.print("Shortest Film is: \n");
+    shortestFilm.display();
+  }
+}
+
+
+class Film
+{
+  String title;
+  int length;
+  String rating;
+  
+  public Film (String sname, int min, String r)
+  {
+    title = sname;
+    length = min;
+    rating = r;
+  }
+  public void display()
+  {
+  	System.out.println("Title of Movie: " + title + "\nLength: " + length + "\nRating: " + rating);
+  }
+  public int getLength()
+  {
+    return length;
+  }
+  public void setLength(int len)
+  {
+    length = ((int)((double)length * (len/100.0))) + length;
+  }
+}
